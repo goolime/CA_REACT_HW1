dd5530402c30540d1abf259aaed6487e00676040
Gal Meir <goolime@gmail.com>
2025-08-22 10:43:23 +0300

    fixes for challenge #5 - added useRef for event handler and changed mouse position in none tracking phase

--- cmps/MouseMonitor.jsx
+++ cmps/MouseMonitor.jsx
@@ -1,22 +1,22 @@
 
-const { useState, useEffect } = React
+const { useState, useEffect , useRef } = React
 
 export function MouseMonitor() {
     const [mousePos, setMousePos] = useState({ x: 0, y: 0 });
     const [isTracking, setIsTracking] = useState(true);
-    const [handleMouseMove, setHandleMouseMove] = useState(() => (event) => {
-        setMousePos({ x: event.clientX, y: event.clientY });
-    });
+    const handleMouseMove = useRef();
 
     useEffect(() => {
-        window.addEventListener("mousemove", handleMouseMove);
+        handleMouseMove.current = (event) => {
+            setMousePos({ x: event.clientX, y: event.clientY });
+        };
+        
+        window.addEventListener("mousemove", handleMouseMove.current);
         
         return () => {
-            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mousemove", handleMouseMove.current);
         };
     }, []);
-
-    
 
     return (
         <div className="mouse-monitor">
@@ -26,12 +26,12 @@ export function MouseMonitor() {
             </div>
             <button onClick={() => {
                 if (isTracking) {
-                    window.removeEventListener("mousemove", handleMouseMove);
+                    window.removeEventListener("mousemove", handleMouseMove.current);
                 } else {
-                    window.addEventListener("mousemove", handleMouseMove);
+                    window.addEventListener("mousemove", handleMouseMove.current);
                 }
                 setIsTracking((prev) => !prev);
-                setMousePos({ x: '-', y: '-' });
+                setMousePos(() => ({ x: '-', y: '-' }));
             }}>{isTracking ? 'Stop Tracking' : 'Start Tracking'}</button>
         </div>
     );
